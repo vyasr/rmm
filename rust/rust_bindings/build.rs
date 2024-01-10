2fc2a4eb0d3218459a1847d361d947f4e606efde
Vyas Ramasubramani <vyasr@nvidia.com>
2024-01-10 00:23:14 +0000

    Compiles and runs!

--- rust/rust_bindings/build.rs
+++ rust/rust_bindings/build.rs
@@ -3,12 +3,12 @@ use cmake::Config;
 
 fn main() {
     // TODO: Probably need to make a static lib of the cpp
-    let dst = Config::new("cpp")
-        .configure_arg("-Drmm_ROOT=/home/coder/rmm/build/latest")
-        .build_target("all")
-        .build();
+    //let dst = Config::new("cpp")
+    //    .configure_arg("-Drmm_ROOT=/home/coder/rmm/build/latest")
+    //    .build_target("all")
+    //    .build();
     cxx_build::bridge("src/main.rs")
         .compile("rust_bindings");
-    println!("cargo:rustc-link-search=native={}/build", dst.display());
-    println!("cargo:rustc-link-lib=static=rmm_rust_bindings");
+    // TODO: Figure out why this is necessary and why corrosion_link_libraries isn't doing the job.
+    println!("cargo:rustc-link-lib=static=rmm_shim");
 }
